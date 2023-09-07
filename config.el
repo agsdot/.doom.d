aeb92025446076b8fab8141ba11463455682f560
Jonathan Lai <jonathanlai@gmail.com>
2023-09-06 21:46:09 -0700

    Update configs according to mostly doom defaults, ivy -> vertico

--- config.el
+++ config.el
@@ -5,40 +5,58 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
-;; (setq doom-theme 'doom-material)
-;; (setq doom-theme 'doom-tomorrow-night)
 (setq doom-theme 'seoul256)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-;; Here are some additional functions/macros that could help you configure Doom:
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -51,6 +69,8 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
@@ -138,25 +158,39 @@
 (defalias 'q! 'evil-quit)
 
 
+(defalias 'fs 'evil-write-all)
+(defalias 'pf 'projectile-find-file)
+
 ;; https://www.reddit.com/r/emacs/comments/hsb0ma/my_own_stupid_spacemacs_clone/
 ;; https://lccambiaghi.github.io/.doom.d/readme.html
-(general-auto-unbind-keys)
+;;(general-auto-unbind-keys)
+;;(map! :leader
+;;      ;;:desc "M-x"                   :n "SPC" #'counsel-M-x
+;;      ;;:desc "ivy resume" :n ":" #'ivy-resume
+;;      ;;:desc "Async shell command"   :n "!"   #'async-shell-command
+;;      ;;:desc "Toggle eshell"         :n "'"   #'+eshell/toggle
+;;      ;;:desc "Open dir in iTerm" :n "oi" #'+macos/open-in-iterm
+;;      ;;:desc "Neotree toggle" :n "nt" #'neotree-toggle
+;;      ;;:desc "Neotree find" :n "nf" #'neotree-find
+;;      ;;;:desc "ace-jump-mode" :n "aj" #'ace-jump-mode
+;;      ;;:desc "evil-write-all" :n "fs" #'evil-write-all
+;;      :desc "projectile-find-file" :n "pf" #'projectile-find-file
+;;
+;;      ;; (:desc "windows" :prefix "w"
+;;      ;;   :desc "popup raise" :n "p" #'+popup/raise)
+;;
+;;      ;; (:desc "project" :prefix "p"
+;;      ;;   :desc "Eshell"               :n "'" #'projectile-run-eshell
+;;      ;;   :desc "Terminal" :n "t" #'projectile-run-vterm )
+;;      )
 (map! :leader
-      :desc "M-x"                   :n "SPC" #'counsel-M-x
-      :desc "ivy resume" :n ":" #'ivy-resume
-      :desc "Async shell command"   :n "!"   #'async-shell-command
-      :desc "Toggle eshell"         :n "'"   #'+eshell/toggle
-      :desc "Open dir in iTerm" :n "oi" #'+macos/open-in-iterm
-      :desc "Neotree toggle" :n "nt" #'neotree-toggle
-      :desc "Neotree find" :n "nf" #'neotree-find
-      :desc "ace-jump-mode" :n "aj" #'ace-jump-mode
+      "SPC" #'execute-extended-command)
 
-      (:desc "windows" :prefix "w"
-        :desc "popup raise" :n "p" #'+popup/raise)
-
-      (:desc "project" :prefix "p"
-        :desc "Eshell"               :n "'" #'projectile-run-eshell
-        :desc "Terminal" :n "t" #'projectile-run-vterm ))
+;; popup dashboard, overwrite what which-key displays
+(after! which-key
+  (which-key-add-key-based-replacements
+    "SPC SPC" "M-x execute-extended-command"
+    "SPC :" "M-x execute-extended-command"))
 
 ;; https://lccambiaghi.github.io/.doom.d/readme.html
 (setq-default
